ecaa7f2dabc438a86710e52c271ad9a929ae984d
Caleb Stokka and Sky Rousse <sky@>
2016-08-30 16:09:01 -0700

    hash location strategy

--- assets/app/boot.ts
+++ assets/app/boot.ts
@@ -3,5 +3,7 @@ import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from "./app.component";
 import {WordService} from './words/word.service';
 import {ROUTER_PROVIDERS} from "angular2/router";
+import {LocationStrategy, HashLocationStrategy} from "angular2/router";
+import {provide} from "angular2/core";
 
-bootstrap(AppComponent, [WordService, ROUTER_PROVIDERS]);
+bootstrap(AppComponent, [WordService, ROUTER_PROVIDERS, provide(LocationStrategy, {useClass: HashLocationStrategy})]);
