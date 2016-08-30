9760f81b46ae2f65e90bcae2fabe2e9babc241a7
Maarja Laineste and Noah Yasskin <maarja@>
2016-08-30 14:45:58 -0700

    add word div background image styles

--- assets/app/words/word.component.ts
+++ assets/app/words/word.component.ts
@@ -4,9 +4,12 @@ import { Word } from './word';
 @Component({
   selector: 'my-word',
   template:`
-      <div class="word" [ngStyle]="{'background-image': 'url(' +  word.image + ')'}">
+      <div class="word" [ngStyle]="{'background-image': 'url(' +  word.image + ')',
+    'background-repeat' : 'no-repeat',
+    'background-size' : 'cover',
+    'background-position' : 'center'}">
       <div class="center-header">
-        <h3>{{ word.name }}</h3>
+        <h2>{{ word.name }}</h2>
       </div>
       </div>
       <div class="definition">
