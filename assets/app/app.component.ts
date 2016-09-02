171a931e09525a2c71e06af31ad33fef3d89438f
Caleb Stokka <caleb.stokka@gmail.com>
2016-09-02 08:13:41 -0700

    remove commented code

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -24,10 +24,5 @@ import {WordInputComponent} from './words/word-input.component';
 export class AppComponent {
   public words: Word[];
   constructor(){
-    // this.words = [
-    //   new Word("Pisan Zapra", "The time needed to eat a banana", "images/pisan_zapra.jpg"),
-    //   new Word("Schadenfreude", "The satisfaction we find in another person’s failure or suffering.", "images/schadenfreude.png"),
-    //   new Word("Age-otori", "The feeling of looking worse after a haircut.", "images/age_otori.jpg")
-    // ];
   }
 }
